303b1d5af3acb7f717a2ec2e6afc74f2cb9bc0af
nikita-t5 <nickita.tashirev@yandex.ru>
2020-03-22 21:39:11 +0300

    error#2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,10 +4,10 @@ public class Calculator {
         return x + y;
     }
     public static int sub(int x, int y){
-        return x - y;
+        return x -/+ y;
     }
     public static int mul(int x, int y){
-        return x * y;
+        return x * y
     }
     public static int div(int x, int y){
         return x / y;
