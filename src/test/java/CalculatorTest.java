7e0b7bfea5e810d9d3ba81e385731fe9d55c6c65
nikita-t5 <nickita.tashirev@yandex.ru>
2020-03-22 18:27:03 +0300

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void sum() {
+        assertEquals( 4, Calculator.sum(2, 2));
+    }
+    @Test
+    void sub() {
+        assertEquals( 4, Calculator.sub(8, 4));
+    }
+
+    @Test
+    void mul() {
+        assertEquals(8,Calculator.mul(2,4));
+    }
+
+    @Test
+    void div() {
+        assertEquals( 4, Calculator.div(40,10));
+    }
+}
